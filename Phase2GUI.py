1d271cf5658754c5dcc620042687030554e20cd4
Dylan Schneider <dylanschneider@Dylans-MacBook-Air.local>
2019-10-08 16:18:06 -0400

    Early stage of a simple gui that allows the user to input file path and then click a button to send the image to the server.

--- Phase2GUI.py
+++ Phase2GUI.py
@@ -1,24 +1,9 @@
+import io
 from tkinter import *
 from UDPServer import UDPServer
 from UDPClient import UDPClient
+from PIL import Image
 from packet_functions import make_packet, send_packets, receive_packets
-
-
-class ClientFrame(Frame):  # Inherits from class Frame
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.client = UDPClient()
-        Label(text='Client Side').pack()
-
-        canvas = Canvas(parent, )
-        Button(parent, text='Send Image',
-               command=lambda s=self.client.client_socket,
-                              p=self.client.packets,
-                              a=self.client.addr_and_port:
-               send_packets(s, p, a)).pack(side='top')
-
-    def show_img(self):
-
 
 
 class ServerFrame(Frame):
@@ -27,12 +12,53 @@ class ServerFrame(Frame):
         self.server = UDPServer()
 
 
+class ClientFrame(Frame):  # Inherits from class Frame
+    def __init__(self, parent):
+        self.client = UDPClient()
+
+        root = Tk()
+        global e
+        e = Entry(root)
+        e.pack()
+        e.focus_set()
+
+        root.title('Enter file name (path required if not in project directory)')
+
+        def send_image():
+            # Get the file name from the user
+            file_name = e.get()
+            # Break down the file to packets
+            p = make_packet(self.client.open_image(file_name))
+            # Define the address of the server
+            a = self.client.addr_and_port
+            # Define the address of the client
+            s = self.client.client_socket
+            # Send the packets to the server
+            send_packets(s, p, a)
+            # Await response from the server
+            packets, server_address = receive_packets(self.client.client_socket)
+
+            # Join the packets back to a bytes object
+            print('CLIENT - All packets have been received from the server')
+            data = b''.join(packets)
+            # Open the grayscale image to confirm the server could modify the original
+            image = Image.open(io.BytesIO(data))
+            print('CLIENT - Packets have been converted back to an image')
+            image.show()
+
+            # Close the client
+            # self.client.client_socket.close()
+
+        b = Button(root, text='Send File', command=send_image)
+        b.pack(side='bottom')
+        root.mainloop()
+
+
 class App(Tk):
     def __init__(self):
         super().__init__()
         ServerFrame(self).pack(side='right')
         ClientFrame(self).pack(side='left')
-        self.mainloop()
 
 
 if __name__ == '__main__':
--- UDPClient.py
+++ UDPClient.py
@@ -1,15 +1,14 @@
 import socket
 from packet_functions import *
-from PIL import Image, ImageFile    # PIL is used for image support in this program.
+from PIL import Image   # PIL is used for image support in this program.
 import io
 
 
 class UDPClient:
-    def __init__(self, file_name: str = 'island.bmp'):
+    def __init__(self):
         """
         Send an image to server, then request a modified image back from the server.
         """
-        self.file_name = file_name      # The name of the file to be opened.
         self.message = b''
         server_address = '127.0.0.1'    # server address; it is the local host in this case
         server_port = 12000             # server port number
@@ -17,36 +16,37 @@ class UDPClient:
         self.client_socket = socket(AF_INET, SOCK_DGRAM)
 
         # Open and show the original image
-
-        self.open_image(file_name)
-        # Convert the image to packets
-        print("CLIENT - Creating packets")
-        self.packets = make_packet(self.message)
-
-        # Send the packets to the server
-        print("CLIENT - Sending packets:")
-        send_packets(self.client_socket, self.packets, self.addr_and_port)
-        print("CLIENT - Finished sending packets")
-
-        # Await for packets coming back from the server
-        packets, server_address = receive_packets(self.client_socket)
-
-        # Join the packets back to a bytes object
-        print('CLIENT - All packets have been received from the server')
-        data = b''.join(packets)
-        # Open the grayscale image to confirm the server could modify the original
-        image = Image.open(io.BytesIO(data))
-        print('CLIENT - Packets have been converted back to an image')
-        image.show()
+        #
+        # self.open_image(file_name)
+        # # Convert the image to packets
+        # print("CLIENT - Creating packets")
+        # self.packets = make_packet(self.message)
+        #
+        # # Send the packets to the server
+        # print("CLIENT - Sending packets:")
+        # send_packets(self.client_socket, self.packets, self.addr_and_port)
+        # print("CLIENT - Finished sending packets")
+        #
+        # # Await for packets coming back from the server
+        # packets, server_address = receive_packets(self.client_socket)
+        #
+        # # Join the packets back to a bytes object
+        # print('CLIENT - All packets have been received from the server')
+        # data = b''.join(packets)
+        # # Open the grayscale image to confirm the server could modify the original
+        # image = Image.open(io.BytesIO(data))
+        # print('CLIENT - Packets have been converted back to an image')
+        # image.show()
 
         # Close the client
-        self.client_socket.close()
+        # self.client_socket.close()
 
-    def open_image(self, file_name: str):
-        with open(self.file_name, 'rb') as image:  # open the file to be transmitted
+    def open_image(self, file_name):
+        with open(file_name, 'rb') as image:  # open the file to be transmitted
             self.message = image.read()
             img = Image.open(io.BytesIO(self.message))
             img.show()
+            return self.message
 
     def __del__(self):
         """
