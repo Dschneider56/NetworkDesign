64bad034c37fb69e614886887bb0cc8a419279f2
roarkem <roarkem123@gmail.com>
2019-10-24 22:59:46 -0400

    WORK IN PROGRESS: added checksum and sequence number generator in make_packet, also modified receive packet to parse out the extra data. Prints a garbled grayscale image currently.

--- packet_functions.py
+++ packet_functions.py
@@ -1,8 +1,10 @@
 from socket import *
 from time import sleep
 
-
 # NOTE: These variables can be changed by simply altering them here. You don't need to change code anywhere else.
+
+SEQNUM_SIZE = 1         # Size of sequence number in bytes.
+CHKSUM_SIZE = 24        # Size of checksum in bytes.
 PACKET_SIZE = 2048      # Size of a packet in bytes.
 TERMINATE = b'\r\n'     # The terminator character sequence.
 
@@ -20,19 +22,88 @@ def send_packets(sock: socket, packets: list, addr_and_port: tuple):
     for packet in packets:
         # print(packet)
         sock.sendto(packet, addr_and_port)      # Send the packet.
-        sleep(0.0001)                           # Small delay so receiver is not overloaded.
+        sleep(0.01)                           # Small delay so receiver is not overloaded.
 
     # print("Sending terminate statement:")
     sock.sendto(TERMINATE, addr_and_port)    # Every packet has been sent, signal the recipient to stop listening.
 
 
-def receive_packets(sock: socket):
+def parse_packet(raw_data: bytes) -> tuple:
+    """
+    From a string of raw data, extract the sequence number, checksum, and data. Return these values in a tuple.
+
+    :param raw_data:    The raw data to parse.
+
+    :return: A tuple containing the sequence number, checksum, and packet contents
+    """
+    seqnum = raw_data[:-SEQNUM_SIZE]
+    chksum = raw_data[-CHKSUM_SIZE:-SEQNUM_SIZE]
+    data = raw_data[:PACKET_SIZE]
+    return data, chksum, seqnum
+
+
+def receive_packets(sock: socket) -> tuple:
     """
     Listen for packets coming in to a socket.
 
     :param sock:    The socket that will be receiving packets.
 
-    :return:        None
+    :return:        the packets along with the address of the sender
+    """
+    packets = []
+    while True:
+        raw_data, return_address = sock.recvfrom(PACKET_SIZE + CHKSUM_SIZE + SEQNUM_SIZE)  # Receive a packet
+
+        packet = parse_packet(raw_data)
+
+        if raw_data == TERMINATE:    # If the TERMINATE character sequence is received, transition is complete.
+            return packets, return_address
+        else:
+            packets.append(raw_data)     # Add the received packet to a list and repeat.
+
+
+def make_packet(data: bytes) -> list:
+    """
+    Given a byte array, split it up into packets of a certain size containing the data, a checksum,
+    and a sequence number.
+
+    :param data:    The byte array that will be split up.
+
+    :return:        A list of packets.
+    """
+    packets: list = []
+    seqnum = 1
+    while len(data) > 0:                                # Keep appending the packets to the packet list.
+        try:
+            raw_packet = data[:PACKET_SIZE]                     # Extract the first "PACKET_SIZE" bytes into packet.
+            data = data[PACKET_SIZE:]
+        except IndexError:
+            raw_packet = data                                   # In this case, the remaining data is less than a packet
+            data = []                                           # set the data to an empty list to break from loop.
+
+        chksum = bytes(format(sum(raw_packet), '024b'),         # Create a checksum for the packet.
+                       'utf-8')
+        seqnum = bytes(str(seqnum ^ 1), 'utf-8')                # Create an alternating sequence number.
+
+        print(seqnum)
+        print(chksum)
+
+        packet = raw_packet + chksum + seqnum                   # Combine seqnum, chksum, & raw_packet into packet
+        packets.append(packet)
+
+        seqnum = int(seqnum)
+    return packets
+
+# --------------------------------------- OLD FUNCTIONS BEFORE MODIFICATION ------------------------------------------ #
+# To restore these functions, simply remove the _OLD suffix and add it to the function above to change out.
+
+def receive_packets_OLD(sock: socket) -> tuple:
+    """
+    Listen for packets coming in to a socket.
+
+    :param sock:    The socket that will be receiving packets.
+
+    :return:        the packets along with the address of the sender
     """
     packets = []
     while True:
@@ -47,14 +118,17 @@ def receive_packets(sock: socket):
             # print(message)
 
 
-def make_packet(data: bytes) -> list:
+def make_packet_OLD(data: bytes) -> list:
     """
-    Given a byte array, split it up into packets of a specified size.
+    Given a byte array, split it up into packets of a certain size containing the data, a checksum,
+    and a sequence number.
 
     :param data:    The byte array that will be split up.
 
     :return:        A list of packets.
     """
+    packets: list = []
+    seqnum = 1
     packets: list = []
     while len(data) >= PACKET_SIZE:             # Keep appending the packets to the packet list
         packets.append(data[:PACKET_SIZE])      # Take up to 'PACKET_SIZE' bytes and add that packet to a list
