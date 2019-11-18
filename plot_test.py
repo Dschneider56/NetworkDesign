60c11c434a3a59c5f5f4d3967e5e87de87780aae
roarkem <roarkem123@gmail.com>
2019-11-17 22:39:59 -0500

    added testing for ack and data loss

--- plot_test.py
+++ plot_test.py
@@ -3,54 +3,69 @@ from Receiver import Receiver
 import numpy as np
 import matplotlib.pyplot as plt
 
-TRIALS = 10
+TRIALS = 3
 TO_PERCENT = 0.65
 
-if __name__ == '__main__':
-    corruption_rates = np.arange(0.0, TO_PERCENT, 0.05)
-    delta_times = []
-    delta_times_data = []
-    delta_times_ack = []
-    Receiver()  # Create receiver thread
+dest = r'C:\\Users\\Roarke\\Desktop\\graphs\\'
 
-    # Evaluate times for regular data
-    for trial in range(TRIALS):
-        #time = []
+corruption_rates = np.arange(0.0, TO_PERCENT, 0.05)
+#delta_times = []
+#delta_times_data = []
+#delta_times_ack = []
+
+def eval_reg_data():
+    delta_times = []
+    for trial in range(20):
+        # time = []
+        r = Receiver()
         c = Sender()  # Create sender object
-        #times.append(c.delta_time)
-        #print(c.delta_time)
+        # times.append(c.delta_time)
+        # print(c.delta_time)
         delta_times.append(c.delta_time)
 
     plt.grid(color='black', linestyle=':', linewidth=1)
-    plt.title(f"Average File Send Time over {TRIALS} Trials\nvs.\nNo Corruption")
+    plt.title(f"Average File Send Time over {20} Trials\nvs.\nNo Corruption")
     plt.xlabel("Trial number")
     plt.ylabel("Completion time (s)")
 
-    plt.plot(range(TRIALS), delta_times)
-    plt.show()
+    plt.plot(range(20), delta_times)
+    plt.savefig(dest + 'no_corruption.png')
+    plt.close()
+    delta_times = []
+    #plt.show()
 
-    # Evaluate times for corrupt data
+def eval_data_corrupt():
+    delta_times_data = []
     for corruption_rate in corruption_rates:
         times = []
         for i in range(TRIALS):
-            c = Sender(data_percent_loss=corruption_rate)  # Create sender object
+            r = Receiver(data_percent_corrupt=corruption_rate)
+            c = Sender()  # Create sender object
             times.append(c.delta_time)
+            print(f'data corrupt trial # {i}: rate at {corruption_rate}')
         delta_times_data.append(np.mean(times))
 
     plt.grid(color='black', linestyle=':', linewidth=1)
-    plt.title(f"Average File Send Time over {TRIALS} Trials\nvs.\nCorruption Percentage of Data")
+    plt.title(f"Average File Send Time over 10 Trials\nvs.\nCorruption Percentage of Data")
     plt.xlabel("Corruption Rate (percent of packets corrupt)")
     plt.ylabel("Average time (s)")
     plt.plot(corruption_rates, delta_times_data)
-    plt.show()
+    plt.savefig(dest + 'data_corruption.png')
+    plt.close()
+    delta_times_data = []
+    #plt.show()
 
+def eval_ack_corrupt():
+    delta_times_ack = []
     # Evaluate times for corrupt ack
     for corruption_rate in corruption_rates:
         times = []
         for i in range(TRIALS):
-            c = Sender(ack_percent_loss=corruption_rate)  # Create sender object
+            r = Receiver(ack_percent_corrupt=corruption_rate)
+            c = Sender()  # Create sender object
 
             times.append(c.delta_time)
+            print(f'ack corrupt trial # {i}: rate at {corruption_rate}')
         delta_times_ack.append(np.mean(times))
 
     plt.grid(color='black', linestyle=':', linewidth=1)
@@ -58,4 +73,69 @@ if __name__ == '__main__':
     plt.xlabel("Corruption Rate (percent of packets corrupt)")
     plt.ylabel("Average time (s)")
     plt.plot(corruption_rates, delta_times_ack)
-    plt.show()
+    plt.savefig(dest + 'ack_corruption.png')
+    plt.close()
+    delta_times_ack = []
+    #plt.show()
+
+def eval_data_loss():
+    delta_times_data_loss = []
+    # Evaluate data loss
+    for corruption_rate in corruption_rates:
+        times = []
+        for i in range(TRIALS):
+            r = Receiver()
+            c = Sender(data_percent_loss=corruption_rate)  # Create sender object
+
+            times.append(c.delta_time)
+            print(f'data loss trial # {i}: rate at {corruption_rate}')
+        delta_times_data_loss.append(np.mean(times))
+
+    plt.grid(color='black', linestyle=':', linewidth=1)
+    plt.title(f"Average File Send Time over {TRIALS} Trials\nvs.\nLoss Percentage of Data")
+    plt.xlabel("Loss Rate (percent of packets data lost)")
+    plt.ylabel("Average time (s)")
+    plt.plot(corruption_rates, delta_times_data_loss)
+    plt.savefig(dest + 'data_loss.png')
+    plt.close()
+    delta_times_data_loss = []
+    #plt.show()
+
+def eval_ack_loss():
+    delta_times_ack_loss = []
+    # Evaluate data loss
+    for corruption_rate in corruption_rates:
+        times = []
+        for i in range(TRIALS):
+            r = Receiver()
+            c = Sender(ack_percent_loss=corruption_rate)  # Create sender object
+            print(f'ack loss trial # {i}: rate at {corruption_rate}')
+            times.append(c.delta_time)
+        delta_times_ack_loss.append(np.mean(times))
+
+    plt.grid(color='black', linestyle=':', linewidth=1)
+    plt.title("Average File Send Time over 10 Trials\nvs.\nLoss Percentage of ACK bit")
+    plt.xlabel("Loss Rate (percent of packets ack lost)")
+    plt.ylabel("Average time (s)")
+    plt.plot(corruption_rates, delta_times_ack_loss)
+    plt.savefig(dest + 'ack_loss.png')
+    plt.close()
+    delta_times_ack_loss = []
+    #plt.show()
+
+if __name__ == '__main__':
+
+    #eval_reg_data()
+   # eval_data_corrupt()
+    eval_ack_corrupt()
+    eval_data_loss()
+    eval_ack_loss()
+
+
+
+
+
+
+    #plt.close()
+    # data
+
